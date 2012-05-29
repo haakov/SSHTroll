b1db9e84bb8ecc4f23be9d60d602d90ca1def5f8
Håkon Vågsether <hauk142@gmail.com>
2012-05-29 17:50:27 +0200

    Commit it all!

---
+++ INSTALL.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# INSTALL.sh - Installer for the SSHTroll Python honeypot
+# Author: Håkon Vågsether <hauk142@gmail.com>
+
+mkdir -p /usr/lib/python2.7/site-packages
+cp sshtroll.py /usr/lib/python2.7/site-packages/
+mkdir -p /usr/bin
+cp sshtroll /usr/bin/
+
+echo ""
+echo""
+echo "If you received no errors above, SSHTroll 1.0 is now installed. Start it with the 'sshtroll' command."
---
+++ sshtroll.py
@@ -0,0 +1,22 @@
+# sshtroll.py - Tiny SSH Python honeypot
+# Author: Håkon Vågsether <hauk142@gmail.com>
+
+import socket
+from time import strftime
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('',22))
+sock.listen(1)
+while True:
+        channel, details = sock.accept()
+        FILE=open("/var/log/sshtroll.log", "a")
+        FILE.write(strftime("%d %b %Y %H:%M:%S:"))
+        FILE.write("\n\t\t\t\t\t")
+        FILE.write(str(details))
+        FILE.write("\n")
+        FILE.close()
+        channel.send("SSH-2.0-SSHTroll")
+        channel.close()
+
+sock.close()
+
